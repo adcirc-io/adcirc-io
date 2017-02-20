a5838e89decef3060a277e1d7dc9146a0e7c7b59
Tristan <atdyer@ncsu.edu>
2017-02-20 16:12:30 -0500

    Working on fort14worker

--- src/fort14worker.js
+++ src/fort14worker.js
@@ -75,6 +75,16 @@ function build_worker () {
 
     var agrid;
     var info_line;
+    var num_nodes;
+    var num_elements;
+    var min_x = Infinity, max_x = -Infinity;
+    var min_y = Infinity, max_y = -Infinity;
+    var min_z = Infinity, max_z = -Infinity;
+
+    var node_array;
+    var node_map = {};
+    var element_array;
+    var element_map = {};
 
     self.addEventListener( 'message', function ( message ) {
 
@@ -85,10 +95,8 @@ function build_worker () {
             case 'read':
 
                 file_reader( message.file )
-                    // .offset( 104 )
-                    .block_size( 300 )
                     .block_callback( parse_data )
-                    .continue_callback( keep_going )
+                    .finished_callback( done )
                     .read();
 
                 break;
@@ -96,6 +104,17 @@ function build_worker () {
         }
 
     });
+
+    function done () {
+        console.log( 'Nodes: ' + num_nodes );
+        console.log( 'Elements: ' + num_elements );
+        console.log( 'X-Range: ' + min_x + '\t' + max_x );
+        console.log( 'Y-Range: ' + min_y + '\t' + max_y );
+        console.log( 'Z-Range: ' + min_z + '\t' + max_z );
+        console.log( line_map );
+        console.log( node_map );
+        console.log( element_map );
+    }
 
     function parse_data ( data ) {
 
@@ -106,9 +125,11 @@ function build_worker () {
         data = line_part + data;
 
         // Perform matching
+        var dat;
         var match;
         while ( ( match = regex_line.exec( data ) ) !== null ) {
 
+            // Read the AGRID line
             if ( line == 0 ) {
 
                 line_map[ 'agrid' ] = file_loc + match.index;
@@ -116,10 +137,40 @@ function build_worker () {
 
             }
 
+            // Read the mesh info line
             else if ( line == 1 ) {
 
                 line_map[ 'info_line' ] = file_loc + match.index;
                 info_line = match[0].trim();
+
+                // Get the number of elements and nodes
+                dat = info_line.match( regex_nonwhite );
+                num_elements = parseInt( dat[0] );
+                num_nodes = parseInt( dat[1] );
+
+                // Allocate the arrays
+                node_array = new Float32Array( 3 * num_nodes );
+                element_array = new Uint32Array( 3 * num_elements );
+
+            }
+
+            else if ( line >= 2 && line < 2 + num_nodes ) {
+
+                if ( line == 2 ) {
+                    line_map[ 'nodes' ] = file_loc + match.index;
+                }
+
+                parse_node_line( match[0] );
+
+            }
+
+            else if ( line >= 2 + num_nodes && line < 2 + num_nodes + num_elements ) {
+
+                if ( line == 2 + num_nodes ) {
+                    line_map[ 'elements' ] = file_loc + match.index;
+                }
+
+                parse_element_line( match[0] );
 
             }
 
@@ -132,15 +183,42 @@ function build_worker () {
         line_part = data.slice( blob_loc );
         file_loc += blob_loc;
 
-        console.log( agrid );
-        console.log( info_line );
+    }
+
+    function parse_node_line ( str ) {
+
+        var dat = str.match( regex_nonwhite );
+        var nn = parseInt( dat[0] );
+        var x = parseFloat( dat[1] );
+        var y = parseFloat( dat[2] );
+        var z = parseFloat( dat[3] );
+
+        if ( x < min_x ) min_x = x;
+        else if ( x > max_x ) max_x = x;
+        if ( y < min_y ) min_y = y;
+        else if ( y > max_y ) max_y = y;
+        if ( z < min_z ) min_z = z;
+        else if ( z > max_z ) max_z = z;
+
+        var node_index = line - 2;
+        node_array[ 3 * node_index ] = x;
+        node_array[ 3 * node_index + 1 ] = y;
+        node_array[ 3 * node_index + 2 ] = z;
+
+        node_map[ nn ] = node_index;
 
     }
 
-    var count = 0;
-    function keep_going () {
-        count += 1;
-        return count < 3;
+    function parse_element_line ( str ) {
+
+        var dat = str.match( regex_nonwhite );
+        var en = parseInt( dat[0] );
+        element_array[ 3 * en ] = parseInt( dat[2] );
+        element_array[ 3 * en + 1 ] = parseInt( dat[3] );
+        element_array[ 3 * en + 2 ] = parseInt( dat[4] );
+
+        element_map[ en ] = line - num_nodes - 2;
+
     }
 
     function post_start () {
